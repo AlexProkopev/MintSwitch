def4c1a638c7d7eab2e4d1c5e515c3a36531f077
AlexProkopev <129211387+AlexProkopev@users.noreply.github.com>
2024-12-05 19:57:32 +0100

    Update Footer.jsx

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import './Footer.css';
 import { Link } from 'react-router-dom';
 import { NOTICE_ROUTE, SITEMAP_ROUTE, TERMS_ROUTE } from '../../components/routes/routes';
 
+
 const Footer = () => {
   return (
     <footer className="footer-container">
@@ -15,6 +16,7 @@ const Footer = () => {
           <a href="https://kurs.expert/" target='blank' className="footer-banner">
             <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgguKkm2wXFtQw-vWU5SLBw7vj66YUzilPL8fXfEPpsml7zAFv90NaVK6f2P535DpoyuLsZlVCGLC-8gmk0vbJMCwycezpUivgtqYVxHxzeK2D-f5FwD71qJEVeQ33OBjqovt3xfxQjK_8/s1600-rw/pic-min.jpg" alt="Безопасный обмен" />
           </a>
+          
           
         </div>
         <div className="footer-info">
